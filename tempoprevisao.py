84ec68df96c25f5ad9082cf667fec86ed51b5538
Raphael Lins <rapharell06@gmail.com>
2022-09-11 00:43:30 -0300

    Add files via upload

---
+++ tempoprevisao.py
@@ -0,0 +1,11 @@
+import pyowm
+
+owm = pyowm.OWM('sua chave qui')
+
+observation = owm.weather_at_place('Brasília,BR')
+
+w = observation.get_weather()
+w.get_wind()
+w.get_humidity()
+w.get_temperature('celsius')
+w
